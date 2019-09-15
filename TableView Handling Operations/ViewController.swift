6a9c15a7d68a0a7ee32739a3a765994b70c1e9fd
Ahmed-Hamza <Ahmed.Hamza@vodafone.com>
2019-09-15 14:28:43 +0200

    Initial Commit

---
+++ TableView Handling Operations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView Handling Operations
+//
+//  Created by Ahmed Sultan  on 9/15/19.
+//  Copyright © 2019 Ahmed Sultan . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
