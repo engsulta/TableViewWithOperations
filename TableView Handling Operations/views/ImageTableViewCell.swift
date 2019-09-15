9c4d238a3ca6cb48a911eb7668601d53887a6941
Ahmed Sultan <en.ahmedsultan@gmail.com>
2019-09-15 18:33:03 +0200

    starting project with new image model and tabel view cell creation

---
+++ TableView Handling Operations/views/ImageTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  TableView Handling Operations
+//
+//  Created by Ahmed Sultan  on 9/15/19.
+//  Copyright © 2019 Ahmed Sultan . All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImageCell: UITableViewCell {
+    
+}
