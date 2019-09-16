d3e5d226395b85061514b816c24f0c3a8611fc4f
Ahmed-Hamza <Ahmed.Hamza@vodafone.com>
2019-09-16 19:16:10 +0200

    adding presenter

---
+++ TableView Handling Operations/presenters/ImageTabelViewPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  ImageTabelViewPresenter.swift
+//  TableView Handling Operations
+//
+//  Created by Ahmed Sultan  on 9/15/19.
+//  Copyright © 2019 Ahmed Sultan . All rights reserved.
+//
+
+import Foundation
+
+
+class ImageTabelViewPresenter {
+    
+    //TODO 
+}
